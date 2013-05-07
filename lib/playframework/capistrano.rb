9e89549101857b0516351557dc02ab0ff0590ab3
Tomás Augusto Müller <tomas.am@gmail.com>
2013-05-07 00:11:31 -0300

    Fixed playframework deploying recipes.

--- lib/playframework/capistrano.rb
+++ lib/playframework/capistrano.rb
@@ -13,71 +13,63 @@ module Capistrano
     _cset(:application, 'my_application')
     _cset(:deploy_to, "/var/lib/#{application}")
 
-    _cset(:log_dir, '/var/log')
-
     _cset(:prod_conf, 'prod.conf')
-    _cset(:project_home, '.')
 
+    # https://github.com/capistrano/capistrano/wiki/2.x-DSL-Action-Invocation-Run
+    default_run_options[:shell] = '/bin/bash --login'
+
+    # Hooks
+    after "deploy", "playframework:setup", "playframework:package"
 
     namespace :playframework do
       task :setup do
-        run "mkdir -p #{deploy_to}"
-        put "#!/bin/bash\nnohup bash -c \"cd #{deploy_to} && target/start $* &>> #{log_dir}/#{application}.log 2>&1\" &> /dev/null &", "#{deploy_to}/start.sh", :mode => '755', :via => :scp
-        put "#!/bin/bash\npid=`cat RUNNING_PID 2> /dev/null`\nif [ \"$pid\" == \"\" ]; then echo '#{application} is not running'; exit 0; fi\necho 'Stopping #{application}...'\nkill -SIGTERM $pid", "#{deploy_to}/stop.sh", :mode => '755', :via => :scp
+        put "#!/bin/bash\nnohup bash -c \"cd #{current_path} && target/start $* &>> #{current_path}/log/#{application}.log 2>&1\" &> /dev/null &", "#{current_path}/start.sh", :mode => '755', :via => :scp
+        put "#!/bin/bash\npid=`cat RUNNING_PID 2> /dev/null`\nif [ \"$pid\" == \"\" ]; then echo '#{application} is not running'; exit 0; fi\necho 'Stopping #{application}...'\nkill -SIGTERM $pid", "#{current_path}/stop.sh", :mode => '755', :via => :scp
+        put "#!/bin/bash\nexport", "#{current_path}/envvars.sh", :mode => '755', :via => :scp
       end
 
-      task :deploy_prod, :roles => :prod do
-        package
-        stop
-        copy_app_files
-        start_prod
+      task :envvars do
+        run [
+          "cd #{current_path}",
+          "./envvars.sh"
+        ].join(" && ")
       end
 
-      task :deploy_dev, :roles => :dev do
-        package
-        stop
-        copy_app_files
-        start_dev
-      end
-
-      task :stop do
-        depend :remote, deploy_to
-        targets = find_servers_for_task(current_task)
-        failed_targets = targets.map do |target|
-          cmd = "ssh -p #{port} #{user}@#{target.host} 'cd #{deploy_to} && ./stop.sh'"
-          target.host unless system cmd
-        end.compact
-        raise "Stopping #{application} failed on #{failed_targets.join(',')}" if failed_targets.any?
-      end
-
-      task :start_prod do
-        targets = find_servers_for_task(current_task)
-        failed_targets = targets.map do |target|
-          cmd = "ssh -p #{port} #{user}@#{target.host} 'cd #{deploy_to} && ./start.sh -Dconfig.resource=#{prod_conf}'"
-          target.host unless system cmd
-        end.compact
-        raise "Starting #{application} failed on #{failed_targets.join(',')}" if failed_targets.any?
-      end
-
-      task :start_dev do
-        targets = find_servers_for_task(current_task)
-        failed_targets = targets.map do |target|
-          cmd = "ssh -p #{port} #{user}@#{target.host} 'cd #{deploy_to} && ./start.sh'"
-          target.host unless system cmd
-        end.compact
-        raise "Starting #{application} failed on #{failed_targets.join(',')}" if failed_targets.any?
+      task :tests do
+        run [
+          "cd #{current_path}",
+          "play test"
+        ].join(" && ")
       end
 
       task :package do
-        system "cd #{project_home} && play clean compile test stage"
-        raise "Error in package task" if not $?.success?
+        run [
+          "cd #{current_path}",
+          "play clean compile stage"
+        ].join(" && ")
       end
 
-      task :copy_app_files do
-        run  "rm -fr #{deploy_to}/target"
-        upload "#{project_home}/target", deploy_to, :via => :scp, :recursive => true
+      task :stop do
+        run [
+          "cd #{current_path}",
+          "./stop.sh"
+        ].join(" && ")
       end
-    end # end namespace playframework
+
+      task :start_prod do
+        run [
+          "cd #{current_path}",
+          "./start.sh -Dconfig.resource=#{prod_conf}"
+        ].join(" && ")
+      end
+
+      task :start_dev do
+        run [
+          "cd #{current_path}",
+          "./start.sh"
+        ].join(" && ")
+      end
+    end
 
   end if Capistrano.const_defined? :Configuration and Capistrano::Configuration.methods.map(&:to_sym).include? :instance
 end # end module Capistrano
